0cd59c770f4462da823005d464588bbf720012a9
WH0M4N <whooman.t@gmail.com>
2026-06-19 14:40:46 +0330

    Make a custom component for food cards and future game cards

---
+++ src/app/components/shared/CustomCard.tsx
@@ -0,0 +1,95 @@
+import { Food, Game } from "@/data/mockData";
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  cardItem: Food | Game;
+  idx: number;
+}
+
+const CustomCard = ({ idx, cardItem }: Props) => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        my: 2,
+      }}
+    >
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          maxWidth: 500,
+          height: 110,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            height: "100%",
+            width: "calc(100% - 45px)",
+            borderRadius: 3,
+            bgcolor: "rgba(255,255,255,0.08)",
+            display: "flex",
+            alignItems: "center",
+
+            ...(idx % 2 === 1
+              ? {
+                  left: 45,
+                  pl: 10,
+                  pr: 3,
+                }
+              : {
+                  right: 45,
+                  pl: 3,
+                  pr: 10,
+                  justifyContent: "flex-end",
+                  textAlign: "right",
+                }),
+          }}
+        >
+          <Box>
+            <Typography sx={{ fontWeight: 700 }}>{cardItem.title}</Typography>
+
+            {cardItem.baseTaste && (
+              <Typography variant="body2">{cardItem.baseTaste}</Typography>
+            )}
+          </Box>
+        </Box>
+
+        {/* Image */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+
+            ...(idx % 2 === 1 ? { left: 0 } : { right: 0 }),
+
+            width: 110,
+            height: 110,
+            borderRadius: "50%",
+            overflow: "hidden",
+            border: "4px solid white",
+            bgcolor: "white",
+            zIndex: 2,
+          }}
+        >
+          <Image
+            src={cardItem.image}
+            alt="Element image"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default CustomCard;
--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,10 +1,18 @@
+import { StaticImageData } from "next/image";
+import randomImage from "../app/images/donar-1-300x300.jpg";
+
 export interface Food {
   id: number;
   title: string;
   desc: string;
+  image: string | StaticImageData;
   baseTaste: string | null;
   price: number;
-  image: string;
+}
+
+export interface Game extends Omit<Food, "baseTaste, price "> {
+  genre: string;
+  player: string;
 }
 
 export const games = [];
@@ -15,7 +23,7 @@ export const foods = [
     desc: "برنج زعفرانی با زرشک و مرغ آبپز شده",
     baseTaste: "ترش و ملایم",
     price: 250000,
-    image: "/images/zereshk-polo.jpg",
+    image: randomImage,
   },
   {
     id: 2,
@@ -23,7 +31,7 @@ export const foods = [
     desc: "خورش سبزیجات معطر با گوشت گوسفند و لوبیا",
     baseTaste: "خوش‌طعم و معطر",
     price: 320000,
-    image: "/images/ghormeh-sabzi.jpg",
+    image: randomImage,
   },
   {
     id: 3,
@@ -31,7 +39,7 @@ export const foods = [
     desc: "سینه مرغ کبابی با زعفران و کره",
     baseTaste: "شور و زعفرانی",
     price: 280000,
-    image: "/images/joojeh-kabab.jpg",
+    image: randomImage,
   },
   {
     id: 4,
@@ -39,7 +47,7 @@ export const foods = [
     desc: "ماست سرد با خیار رنده شده و نعنا",
     baseTaste: "ترش و خنک",
     price: 90000,
-    image: "/images/mast-o-khiar.jpg",
+    image: randomImage,
   },
   {
     id: 5,
@@ -47,7 +55,7 @@ export const foods = [
     desc: "شیرینی سرخ کرده با شربت زعفرانی",
     baseTaste: "شیرین و ترد",
     price: 150000,
-    image: "/images/zolbia-bamieh.jpg",
+    image: randomImage,
   },
   {
     id: 6,
@@ -55,7 +63,7 @@ export const foods = [
     desc: "آش غلیظ با رشته، حبوبات و کشک",
     baseTaste: "ملایم و کشکی",
     price: 180000,
-    image: "/images/ash-reshteh.jpg",
+    image: randomImage,
   },
   {
     id: 7,
@@ -63,7 +71,7 @@ export const foods = [
     desc: "گوشت گوسفند کبابی با ادویه مخصوص",
     baseTaste: "شور و گوشتی",
     price: 350000,
-    image: "/images/kabab-barg.jpg",
+    image: randomImage,
   },
   {
     id: 8,
@@ -71,7 +79,7 @@ export const foods = [
     desc: "برنج زعفرانی با لایه ترد و مرغ",
     baseTaste: "زعفرانی و ملایم",
     price: 290000,
-    image: "/images/tah-chin.jpg",
+    image: randomImage,
   },
   {
     id: 9,
@@ -79,7 +87,7 @@ export const foods = [
     desc: "نودل نشاسته‌ای با شربت گلاب و لیمو",
     baseTaste: "شیرین و خنک",
     price: 120000,
-    image: "/images/faloodeh.jpg",
+    image: randomImage,
   },
   {
     id: 10,
@@ -87,6 +95,6 @@ export const foods = [
     desc: "خورش سنتی با گوشت، نخود و سیب‌زمینی در دیگ سنگی",
     baseTaste: "شور و مقوی",
     price: 200000,
-    image: "/images/dizi.jpg",
+    image: randomImage,
   },
 ];
