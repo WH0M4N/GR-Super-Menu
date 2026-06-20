566d8c31ca3a41c28c48bf3c51014ece93afe9d0
WH0M4N <whooman.t@gmail.com>
2026-06-20 21:18:46 +0330

    Choose the CategoryHorizMenu background color and make it rtl

--- src/app/components/CategoryHorizMenu.tsx
+++ src/app/components/CategoryHorizMenu.tsx
@@ -15,7 +15,8 @@ const CategoryHorizMenu = ({ categories, scrollToCategory }: Props) => {
         width: "100%",
         maxWidth: 500,
         zIndex: 100,
-        bgcolor: "background.default",
+        bgcolor: "rgba(255,255,255,0.07)",
+
         display: "flex",
         gap: 1,
         overflowX: "auto",
@@ -23,10 +24,13 @@ const CategoryHorizMenu = ({ categories, scrollToCategory }: Props) => {
         whiteSpace: "nowrap",
         py: 1,
         px: 1,
+        borderRadius: "6px",
 
         "&::-webkit-scrollbar": {
           display: "none",
         },
+
+        direction: "rtl",
       }}
     >
       {categories.map((category) => (
@@ -38,7 +42,7 @@ const CategoryHorizMenu = ({ categories, scrollToCategory }: Props) => {
             px: 2,
             py: 1,
             borderRadius: 20,
-            bgcolor: "rgba(255,255,255,0.08)",
+            bgcolor: "background.paper",
             cursor: "pointer",
             whiteSpace: "nowrap",
           }}
