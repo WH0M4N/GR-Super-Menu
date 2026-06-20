96a2764562e602d659b58105abc2d481b310e774
WH0M4N <whooman.t@gmail.com>
2026-06-20 22:46:38 +0330

    Remove FoodCard component

--- src/app/components/FoodCard.tsx
+++
@@ -1,8 +0,0 @@
-import { Food } from "@/data/mockData";
-import CustomCard from "./shared/CustomCard";
-
-const FoodCard = ({ food, foodIdx }: { food: Food; foodIdx: number }) => {
-  return <CustomCard cardItem={food} idx={foodIdx} />;
-};
-
-export default FoodCard;
