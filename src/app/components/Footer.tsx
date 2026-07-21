ada1f113591fc4ea53f2b53d9b6a9067b820d8ad
WH0M4N <whooman.t@gmail.com>
2026-07-21 18:48:43 +0330

    Add the instagram, waza and telegram links to the homepage

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { IoCallOutline } from "react-icons/io5";
 import { RiTelegram2Line } from "react-icons/ri";
 import { FaInstagram } from "react-icons/fa6";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 
 const Footer = () => {
   return (
@@ -36,12 +37,40 @@ const Footer = () => {
       <div
         style={{
           display: "flex",
-          gap: 2,
+          gap: 10,
           alignItems: "center",
         }}
       >
-        <RiTelegram2Line size={28} color="rgba(255,255,255,0.75)" />
-        <FaInstagram size={28} color="rgba(255,255,255,0.75)" />
+        <a
+          href="https://t.me/Gamerepublic_complex"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <RiTelegram2Line size={28} color="rgba(255,255,255,0.75)" />
+        </a>
+        <a
+          href="https://www.instagram.com/gamerepublic_complex?igsh=N3RpOHV0aGpoNXh6"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <FaInstagram size={28} color="rgba(255,255,255,0.75)" />
+        </a>
+        <a
+          href="https://www.waze.com/en-GB/live-map/directions?to=ll.35.78644%2C51.377542&utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAb21jcATMa6RleHRuA2FlbQIxMQBzcnRjBmFwcF9pZA81NjcwNjczNDMzNTI0MjcAAaefDdoxFKtT5GV_qxCBUKLaC5IEGZUATap6vpL3WxgVLQxrwQsMbjdtJAGYIA_aem_ZeEdgIdMWR9Jq-7m4h97aA"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <HiOutlineLocationMarker size={28} color="rgba(255,255,255,0.75)" />
+        </a>
       </div>
     </div>
   );
